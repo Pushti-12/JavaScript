55a6bf70e92ee0d346e24efd5d8e2c151178ecb3
Pushti Agrawal <pushti1203@gmail.com>
2026-01-30 15:01:39 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,39 @@
+"use strict"; //treats all js code as newer version of js
+
+// alert(3 + 3) //we are using nodejs not browser
+
+console.log(3
+    +3 // code readability should be high
+);
+
+console.log("pushti");
+
+let name = "Pushti"
+let age = 17
+let islogin = true
+
+
+//number => 2 to power 53
+// bigint - stocks flipkart etc
+// string=> ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+
+//object
+
+
+console.log(typeof age);
+console.log(typeof name);
+console.log(typeof islogin);
+
+
+console.log(typeof null);  //interview questions
+
+
+//object 
+
+console.log(typeof undefined); //undefined
+
