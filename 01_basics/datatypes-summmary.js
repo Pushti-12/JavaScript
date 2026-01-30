932225d2dc3a72ee469a896b1bd1b6eef7176f4b
Pushti Agrawal <pushti1203@gmail.com>
2026-01-30 17:20:37 +0000

    comparison and datatypes summary

---
+++ 01_basics/datatypes-summmary.js
@@ -0,0 +1,38 @@
+//Primitive 
+
+// 7 types : String, Number ,Boolean, Null,Undefined,Symbol,BigInt
+
+const score = 100
+const scoreValue = 100
+
+const IsLogggedIn = false
+const outSideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+
+console.log(id === another);
+
+
+const BigNumber = 2343354657689798e232443546789
+
+
+
+
+// Reference(Non primitive)
+
+//Array, Object, Functions
+
+
+const alphabets = ['A','B']
+
+
+let myObj ={
+    name: "Pushti"
+}
+
+const myfun = function(){
+    console.log("Hello");
+}
